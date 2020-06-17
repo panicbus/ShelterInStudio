1feda35b581fd98a33774c3167fb1f206d91ae80
Nico Crisafulli <elementalair@gmail.com>
2020-06-17 11:58:31 -0700

    new artist, updating artists w content, new participate page

---
+++ js/submitForm.js
@@ -0,0 +1,39 @@
+// moving the field labels up on focus
+[].slice.call( document.querySelectorAll( '.input' ) ).forEach( function( inputEl ) {
+    if( inputEl.value.trim() !== '' ) {
+        classie.add( inputEl.parentNode, 'input--filled' );
+    }
+    inputEl.addEventListener( 'focus', onInputFocus );
+    inputEl.addEventListener( 'blur', onInputBlur );
+} );
+
+function onInputFocus( ev ) {
+      classie.add( ev.target.parentNode, 'input--filled' );
+}
+
+function onInputBlur( ev ) {
+      if( ev.target.value.trim() === '' ) {
+            classie.remove( ev.target.parentNode, 'input--filled' );
+      }
+}
+
+// copy question text to clipboard
+function copyToClip(str) {
+	var textarea = $('.textarea');
+  function listener(e) {
+    e.clipboardData.setData('text/plain', str);
+    e.preventDefault();
+  }
+  document.addEventListener('copy', listener);
+  document.execCommand('copy');
+  document.removeEventListener('copy', listener);
+};
+
+$('.fa-clipboard').on('click', function(){
+	$(this).addClass('the-pulse');
+	$(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend',   
+    function(e) {
+    // code to execute after animation ends
+      $(this).removeClass('the-pulse');
+    });
+})
