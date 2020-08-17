322f4e3022bc0157ab865de7e244fae1cdbca4c3
Nico Crisafulli <elementalair@gmail.com>
2020-08-17 15:34:42 -0700

    reverting filter test

--- js/isotopeFilter.js
+++ js/isotopeFilter.js
@@ -1,32 +1,32 @@
-// quick search regex
-var qsRegex;
+// // quick search regex
+// var qsRegex;
 
-// init Isotope
-var $grid = $('.grid').isotope({
-  itemSelector: '.grid__item',
-  // layoutMode: 'fitRows',
-  filter: function() {
-    return qsRegex ? $(this).text().match( qsRegex ) : true;
-  }
-});
+// // init Isotope
+// var $grid = $('.grid').isotope({
+//   itemSelector: '.grid__item',
+//   // layoutMode: 'fitRows',
+//   filter: function() {
+//     return qsRegex ? $(this).text().match( qsRegex ) : true;
+//   }
+// });
 
-// use value of search field to filter
-var $quicksearch = $('.quicksearch').keyup( debounce( function() {
-  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
-  $grid.isotope();
-}, 200 ) );
+// // use value of search field to filter
+// var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+//   qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+//   $grid.isotope();
+// }, 200 ) );
 
-// debounce so filtering doesn't happen every millisecond
-function debounce( fn, threshold ) {
-  var timeout;
-  threshold = threshold || 100;
-  return function debounced() {
-    clearTimeout( timeout );
-    var args = arguments;
-    var _this = this;
-    function delayed() {
-      fn.apply( _this, args );
-    }
-    timeout = setTimeout( delayed, threshold );
-  };
-}
+// // debounce so filtering doesn't happen every millisecond
+// function debounce( fn, threshold ) {
+//   var timeout;
+//   threshold = threshold || 100;
+//   return function debounced() {
+//     clearTimeout( timeout );
+//     var args = arguments;
+//     var _this = this;
+//     function delayed() {
+//       fn.apply( _this, args );
+//     }
+//     timeout = setTimeout( delayed, threshold );
+//   };
+// }
