f3293a9e5648a3ac55082c3c9ece9671ded4c6a0
Nico Crisafulli <elementalair@gmail.com>
2020-08-26 13:18:10 -0700

    adding clear quicksearch feature

--- js/isotopeFilter.js
+++ js/isotopeFilter.js
@@ -6,29 +6,34 @@ var $grid = $('.grid').isotope({
   itemSelector: '.grid__item',
   layoutMode: 'fitRows',
   filter: function() {
-    return qsRegex ? $(this).text().match( qsRegex ) : true;
+    return qsRegex ? $(this).text().match(qsRegex) : true;
   }
 });
 
 // use value of search field to filter
-var $quicksearch = $('.quicksearch').keyup( debounce( function() {
-  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+var $quicksearch = $('.quicksearch').keyup(debounce(function() {
+  qsRegex = new RegExp($quicksearch.val(), 'gi');
   $grid.isotope();
-}, 200 ) );
+}, 200));
 
 // debounce so filtering doesn't happen every millisecond
-function debounce( fn, threshold ) {
+function debounce(fn, threshold) {
   var timeout;
   threshold = threshold || 100;
-  return function debounced() {
-    clearTimeout( timeout );
+  return function debounced(){
+    clearTimeout(timeout);
     var args = arguments;
     var _this = this;
+    if (!$('.qs-clear').length){
+      $('.qs-container').append('<div class="qs-clear">&times;</div>');
+    } else if ($('.quicksearch').val() === '') {
+      $('.qs-clear').remove();
+    }
     function delayed() {
-      fn.apply( _this, args );
+      fn.apply(_this, args);
 	    checkResults();
     }
-    timeout = setTimeout( delayed, threshold );
+    timeout = setTimeout(delayed, threshold);
   };
 }
 
@@ -43,3 +48,12 @@ function checkResults(){
     $('.no-results-hidden').fadeIn(350);
   }
 }
+
+// clear the quicksearch with x click 
+var $quicksearchEmpty = $(document).on('click', '.qs-clear', debounce(function() {
+  qsRegex = new RegExp( $quicksearchEmpty.val(), 'gi');
+  $('.qs-clear').remove();
+  $('.quicksearch').val('');
+  $grid.isotope();
+}, 200));
+
